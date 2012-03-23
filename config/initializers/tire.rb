f2d6047afdbb82603fc8e0505477b0178763d6e2
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2012-03-23 14:38:31 +0100

    new patch for bonsai/heroku

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,37 +1,37 @@
 require 'tire'
 
-# Horrible monckey patch for bonzai to avoid double / in URL
-# Should make a push request
-module Tire
-  class Index
-    def store(*args)
-      document, options = args
-      type = get_type_from_document(document)
+# Monkey-patch for bonsai.io on heroku
+class HerokuRestClient < Tire::HTTP::Client::RestClient
+	class << self
+		def cleanup_url(url) 
+			# remove duplicated // but not the one in http://
+			url[0...7] + url[7..-1].squeeze('/')
+		end
 
-      if options
-        percolate = options[:percolate]
-        percolate = "*" if percolate === true
-      end
+	  def get(url, data=nil)
+	  	super(cleanup_url(url), data)
+	  end
 
-      id       = get_id_from_document(document)
-      document = convert_document_to_json(document)
+	  def post(url, data)
+	  	super(cleanup_url(url), data)
+	  end
 
-      # Change that to avoid double / in URL, bonsai.io don't like that
-      url  = "#{Configuration.url}#{(id ? "/#{@name}/#{type}/#{id}" : "/#{@name}/#{type}/").squeeze('/')}"
-      url += "?percolate=#{percolate}" if percolate
+	  def put(url, data)
+	  	super(cleanup_url(url), data)
+	  end
 
-      @response = Configuration.client.post url, document
-      MultiJson.decode(@response.body)
+	  def delete(url)
+	  	super(cleanup_url(url))
+	  end
 
-    ensure
-      curl = %Q|curl -X POST "#{url}" -d '#{document}'|
-      logged([type, id].join('/'), curl)
-    end
-  end
+	  def head(url)
+	  	super(cleanup_url(url))
+	  end
+	end
 end
 
 Tire.configure do
    logger STDERR
    url(YAML::load(File.open(Rails.root.join("./config/tire.yml")))[Rails.env]["url"])
-end
-Tire::Model::Search.index_prefix "#{Rails.env}".downcase unless Rails.env.production?
+   client(HerokuRestClient)
+ end
