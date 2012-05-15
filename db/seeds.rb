d338893c7d562244a79ef7ed3c1bde643a26af6f
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2012-05-15 10:28:17 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 # encoding: utf-8
-User.destroy_all
-user = User.new :email => 'sgruhier@gmail.com', :password => "azeaze", :password_confirmation => "azeaze"
-user.admin = true
-user.save!
+
+# User.destroy_all
+# user = User.new :email => 'sgruhier@gmail.com', :password => "azeaze", :password_confirmation => "azeaze"
+# user.admin = true
+# user.save!
+
+user = User.first
 
 Medication.destroy_all
 m = Medication.new name: "ibuprofene", generic_name: "ibuprofene"
